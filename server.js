a072bf05744c824223515debb2b1be148bdf55cd
hamdi <76883216+charradihamdi@users.noreply.github.com>
2022-03-11 19:46:56 +0100

    configuration

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ require('./config/db');
 const cors = require('cors');
 
 const app = express();
-
+//configuration
 app.use(cors());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
