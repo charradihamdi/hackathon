09677ee0c5107551f62cf7d08716147669d76c48
hamdi <76883216+charradihamdi@users.noreply.github.com>
2022-03-11 19:42:35 +0100

    fixe issue

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,8 +1,6 @@
 const postModel = require("../models/post.model");
 const ObjectID = require("mongoose").Types.ObjectId;
 const fs = require("fs");
-const { promisify } = require("util");
-const pipeline = promisify(require("stream").pipeline);
 module.exports.readPost = (req, res) => {
  return res.status(400).send("try test ");
 };
